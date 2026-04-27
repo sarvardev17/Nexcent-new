17fb3ef1e77066a74d78bc4399ab453c09f4d360
sarvardev17 <sarvarpnm7@gmail.com>
2026-04-27 17:52:33 +0500

    istalgan commit yozing

--- main.js
+++ main.js
@@ -10,3 +10,38 @@ bars.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   menu.classList.remove("show-menu");
 });
+
+// ===== CAROUSEL =====
+let carouselInner = document.querySelector(".carousel__inner");
+let dots = document.querySelectorAll(".dot");
+let current = 0;
+
+// Slaydni chiqarish funksiyasi
+function changeSlide() {
+  carouselInner.style.transform = `translateX(-${current * 100}%)`;
+
+  dots.forEach(function (dot) {
+    dot.classList.remove("active");
+  });
+
+  dots[current].classList.add("active");
+}
+
+// Nuqta bosilganda
+dots.forEach(function (dot, index) {
+  dot.addEventListener("click", function () {
+    current = index;
+    changeSlide();
+  });
+});
+
+// Avtomatik almashish
+setInterval(function () {
+  current++;
+
+  if (current === dots.length) {
+    current = 0;
+  }
+
+  changeSlide();
+}, 3000);
